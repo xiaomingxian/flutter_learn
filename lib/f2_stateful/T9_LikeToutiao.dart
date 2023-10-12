d8b757fd023267c9f7dfb15a849a74c9a3a84bef
xianxiaoming <934792834@qq.com>
2023-10-12 23:19:17 +0800

    _tabController监听点击 活动

--- lib/f2_stateful/T9_LikeToutiao.dart
+++ lib/f2_stateful/T9_LikeToutiao.dart
@@ -26,9 +26,27 @@ class _LikeTouTiaoState extends State<LikeTouTiao>
   final List<Widget> _tabBarViews = [];
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    //销毁事件
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
+    //监听tabController改变事件
+    _tabController.addListener(() {
+      //打印2次一次点击 一次离开
+      //滑动的时候是一次
+      // print('监听到索引:${_tabController.index}');
+      if (_tabController.animation!.value == _tabController.index) {
+        //_tabController.animation!.value   _tabController.animation 如果不为空 获取其value
+        print('监听到索引:${_tabController.index}');
+      }
+    });
     _tabs.add(const Tab(
       child: Text(
         "同城",
@@ -42,114 +60,207 @@ class _LikeTouTiaoState extends State<LikeTouTiao>
       child: Text("推荐", style: TextStyle(color: Colors.black)),
     ));
 
-    _tabBarViews.add(
-        KeepVisibleRecord(child:
-        ListView(children: const [
+    _tabBarViews.add(KeepVisibleRecord(
+        child: ListView(children: const [
+      ListTile(
+        title: Text(
+          "同城内容1",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+      ListTile(
+        title: Text(
+          "同城内容2",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+      ListTile(
+        title: Text(
+          "同城内容3",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+      ListTile(
+        title: Text(
+          "同城内容4",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+      ListTile(
+        title: Text(
+          "同城内容5",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+      ListTile(
+        title: Text(
+          "同城内容6",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+      ListTile(
+        title: Text(
+          "同城内容7",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+      ListTile(
+        title: Text(
+          "同城内容8",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+      ListTile(
+        title: Text(
+          "同城内容9",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+      ListTile(
+        title: Text(
+          "同城内容10",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+      ListTile(
+        title: Text(
+          "同城内容11",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+    ])));
+    _tabBarViews.add(KeepVisibleRecord(
+      child: ListView(
+        children: const [
           ListTile(
-            title: Text("同城内容1", style: TextStyle(fontSize: 50),),
+            title: Text(
+              "这里是关注内容1",
+              style: TextStyle(fontSize: 50),
+            ),
           ),
           ListTile(
-            title: Text("同城内容2", style: TextStyle(fontSize: 50),),
+            title: Text(
+              "这里是关注内容2",
+              style: TextStyle(fontSize: 50),
+            ),
           ),
           ListTile(
-            title: Text("同城内容3", style: TextStyle(fontSize: 50),),
+            title: Text(
+              "这里是关注内容3",
+              style: TextStyle(fontSize: 50),
+            ),
           ),
           ListTile(
-            title: Text("同城内容4", style: TextStyle(fontSize: 50),),
+            title: Text(
+              "这里是关注内容4",
+              style: TextStyle(fontSize: 50),
+            ),
           ),
           ListTile(
-            title: Text("同城内容5", style: TextStyle(fontSize: 50),),
+            title: Text(
+              "这里是关注内容5",
+              style: TextStyle(fontSize: 50),
+            ),
           ),
           ListTile(
-            title: Text("同城内容6", style: TextStyle(fontSize: 50),),
+            title: Text(
+              "这里是关注内容6",
+              style: TextStyle(fontSize: 50),
+            ),
           ),
           ListTile(
-            title: Text("同城内容7", style: TextStyle(fontSize: 50),),
+            title: Text(
+              "这里是关注内容7",
+              style: TextStyle(fontSize: 50),
+            ),
           ),
           ListTile(
-            title: Text("同城内容8", style: TextStyle(fontSize: 50),),
+            title: Text(
+              "这里是关注内容8",
+              style: TextStyle(fontSize: 50),
+            ),
           ),
           ListTile(
-            title: Text("同城内容9", style: TextStyle(fontSize: 50),),
-          ), ListTile(
-            title: Text("同城内容10", style: TextStyle(fontSize: 50),),
-          ), ListTile(
-            title: Text("同城内容11", style: TextStyle(fontSize: 50),),
+            title: Text(
+              "这里是关注内容9",
+              style: TextStyle(fontSize: 50),
+            ),
           ),
-        ])));
-    _tabBarViews.add(
-        KeepVisibleRecord(child:
-        ListView(
-          children: const [
-            ListTile(
-              title: Text("这里是关注内容1", style: TextStyle(fontSize: 50),),
+          ListTile(
+            title: Text(
+              "这里是关注内容10",
+              style: TextStyle(fontSize: 50),
             ),
-            ListTile(
-              title: Text("这里是关注内容2", style: TextStyle(fontSize: 50),),
+          ),
+        ],
+      ),
+    ));
+    _tabBarViews.add(KeepVisibleRecord(
+      child: ListView(
+        children: const [
+          ListTile(
+            title: Text(
+              "这里是推荐内容1",
+              style: TextStyle(fontSize: 50),
             ),
-            ListTile(
-              title: Text("这里是关注内容3", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text(
+              "这里是推荐内容2",
+              style: TextStyle(fontSize: 50),
             ),
-            ListTile(
-              title: Text("这里是关注内容4", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text(
+              "这里是推荐内容3",
+              style: TextStyle(fontSize: 50),
             ),
-            ListTile(
-              title: Text("这里是关注内容5", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text(
+              "这里是推荐内容4",
+              style: TextStyle(fontSize: 50),
             ),
-            ListTile(
-              title: Text("这里是关注内容6", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text(
+              "这里是推荐内容5",
+              style: TextStyle(fontSize: 50),
             ),
-            ListTile(
-              title: Text("这里是关注内容7", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text(
+              "这里是推荐内容6",
+              style: TextStyle(fontSize: 50),
             ),
-            ListTile(
-              title: Text("这里是关注内容8", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text(
+              "这里是推荐内容7",
+              style: TextStyle(fontSize: 50),
             ),
-            ListTile(
-              title: Text("这里是关注内容9", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text(
+              "这里是推荐内容8",
+              style: TextStyle(fontSize: 50),
             ),
-            ListTile(
-              title: Text("这里是关注内容10", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text(
+              "这里是推荐内容9",
+              style: TextStyle(fontSize: 50),
             ),
-          ],
-        ),));
-    _tabBarViews.add(
-        KeepVisibleRecord(child: ListView(
-          children: const [
-            ListTile(
-              title: Text("这里是推荐内容1", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text(
+              "这里是推荐内容10",
+              style: TextStyle(fontSize: 50),
             ),
-            ListTile(
-              title: Text("这里是推荐内容2", style: TextStyle(fontSize: 50),),
-            ),
-            ListTile(
-              title: Text("这里是推荐内容3", style: TextStyle(fontSize: 50),),
-            ),
-            ListTile(
-              title: Text("这里是推荐内容4", style: TextStyle(fontSize: 50),),
-            ),
-            ListTile(
-              title: Text("这里是推荐内容5", style: TextStyle(fontSize: 50),),
-            ),
-            ListTile(
-              title: Text("这里是推荐内容6", style: TextStyle(fontSize: 50),),
-            ),
-            ListTile(
-              title: Text("这里是推荐内容7", style: TextStyle(fontSize: 50),),
-            ),
-            ListTile(
-              title: Text("这里是推荐内容8", style: TextStyle(fontSize: 50),),
-            ),
-            ListTile(
-              title: Text("这里是推荐内容9", style: TextStyle(fontSize: 50),),
-            ),
-            ListTile(
-              title: Text("这里是推荐内容10", style: TextStyle(fontSize: 50),),
-            ),
-          ],
-        ),)
-
-    );
+          ),
+        ],
+      ),
+    ));
   }
 
   @override
@@ -161,6 +272,10 @@ class _LikeTouTiaoState extends State<LikeTouTiao>
             backgroundColor: const Color.fromARGB(255, 253, 247, 247),
             elevation: 1,
             title: TabBar(
+              onTap: (index){
+                //只会监听点击事件 监听不到滑动事件
+                print("tabBar监听点击事件获取到的索引${index}");
+              },
               //控制器  TabBar and  TabBarView 都得有
               controller: _tabController,
               //选中的颜色
@@ -179,7 +294,6 @@ class _LikeTouTiaoState extends State<LikeTouTiao>
     );
   }
 }
-
 
 ///定义缓存组件 缓存浏览位置
 ///widget 是 StatefulWidget 的一个属性 表示 当前widget
