c9755c2fdda673cab75eca2466e64f33a03fe790
xianxiaoming <934792834@qq.com>
2023-10-12 22:58:14 +0800

    做页面缓存 保持浏览位置

--- lib/f2_stateful/T9_LikeToutiao.dart
+++ lib/f2_stateful/T9_LikeToutiao.dart
@@ -42,31 +42,182 @@ class _LikeTouTiaoState extends State<LikeTouTiao>
       child: Text("推荐", style: TextStyle(color: Colors.black)),
     ));
 
-    _tabBarViews.add(Center(
-      child: Text("这里是同城内容"),
-    ));
-    _tabBarViews.add(Center(
-      child: Text("这里是关注内容"),
-    ));
-    _tabBarViews.add(Center(
-      child: Text("这里是推荐内容"),
-    ));
+    _tabBarViews.add(
+        KeepVisibleRecord(child:
+        ListView(children: const [
+          ListTile(
+            title: Text("同城内容1", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text("同城内容2", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text("同城内容3", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text("同城内容4", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text("同城内容5", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text("同城内容6", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text("同城内容7", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text("同城内容8", style: TextStyle(fontSize: 50),),
+          ),
+          ListTile(
+            title: Text("同城内容9", style: TextStyle(fontSize: 50),),
+          ), ListTile(
+            title: Text("同城内容10", style: TextStyle(fontSize: 50),),
+          ), ListTile(
+            title: Text("同城内容11", style: TextStyle(fontSize: 50),),
+          ),
+        ])));
+    _tabBarViews.add(
+        KeepVisibleRecord(child:
+        ListView(
+          children: const [
+            ListTile(
+              title: Text("这里是关注内容1", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是关注内容2", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是关注内容3", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是关注内容4", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是关注内容5", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是关注内容6", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是关注内容7", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是关注内容8", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是关注内容9", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是关注内容10", style: TextStyle(fontSize: 50),),
+            ),
+          ],
+        ),));
+    _tabBarViews.add(
+        KeepVisibleRecord(child: ListView(
+          children: const [
+            ListTile(
+              title: Text("这里是推荐内容1", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是推荐内容2", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是推荐内容3", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是推荐内容4", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是推荐内容5", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是推荐内容6", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是推荐内容7", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是推荐内容8", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是推荐内容9", style: TextStyle(fontSize: 50),),
+            ),
+            ListTile(
+              title: Text("这里是推荐内容10", style: TextStyle(fontSize: 50),),
+            ),
+          ],
+        ),)
+
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(40),
-        child: TabBar(
-          controller: _tabController,
-          tabs: _tabs,
-        ),
-      ),
+          preferredSize: Size.fromHeight(50),
+          child: AppBar(
+            backgroundColor: const Color.fromARGB(255, 253, 247, 247),
+            elevation: 1,
+            title: TabBar(
+              //控制器  TabBar and  TabBarView 都得有
+              controller: _tabController,
+              //选中的颜色
+              indicatorColor: Colors.red,
+              //没选中的颜色
+              unselectedLabelColor: Colors.lightGreen,
+              //indicatorSize:TabBar指示器大小.label
+              indicatorSize: TabBarIndicatorSize.label,
+              tabs: _tabs,
+            ),
+          )),
       body: TabBarView(
         controller: _tabController,
         children: _tabBarViews,
       ),
     );
+  }
+}
+
+
+///定义缓存组件 缓存浏览位置
+///widget 是 StatefulWidget 的一个属性 表示 当前widget
+///State可以通过widget属性访问对应的Widget实例。所以这里的widget就是KeepVisibleRecord widget,通过它可以获取到KeepVisibleRecord的child和keepAlive等属性。
+class KeepVisibleRecord extends StatefulWidget {
+  const KeepVisibleRecord(
+      {Key? key, @required this.child, this.keepAlive = true})
+      : super(key: key);
+
+  final Widget? child;
+  final bool keepAlive;
+
+  @override
+  _KeepVisibleRecordState createState() => _KeepVisibleRecordState();
+}
+
+class _KeepVisibleRecordState extends State<KeepVisibleRecord>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    //后面加！表示断言 不为空 如果为空则抛出异常
+    return widget.child!;
+  }
+
+  @override
+  // TODO: implement wantKeepAlive
+  // bool get wantKeepAlive => throw UnimplementedError();
+  bool get wantKeepAlive => widget.keepAlive;
+
+  @override
+  void didUpdateWidget(covariant KeepVisibleRecord oldWidget) {
+    //这段代码的作用是在widget的keepAlive属性改变时,手动通知框架更新State的缓存状态,从而使KeepAlive生效。
+    // 它利用了didUpdateWidget钩子,比较老旧widget值判断keepAlive是否变化,如果变化就调用updateKeepAlive来更新状态。
+    //我的理解 如果老的widget的状态与新的不同 说明数据发生变化了 需要通知框架去刷新数据
+    if (oldWidget.keepAlive != widget.keepAlive) {
+      // keepAlive 状态需要更新，实现在 AutomaticKeepAliveClientMixin 中
+      updateKeepAlive();
+    }
+    super.didUpdateWidget(oldWidget);
   }
 }
