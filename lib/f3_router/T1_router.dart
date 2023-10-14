8003964e983223724708c6ef7b60146da18a9378
xianxiaoming <934792834@qq.com>
2023-10-14 15:30:15 +0800

    路由传参

--- lib/f3_router/T1_router.dart
+++ lib/f3_router/T1_router.dart
@@ -4,7 +4,7 @@ import 'package:flutter_learn/f3_router/T1_searchPage.dart';
 main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: const Text("路由demo")),
+      appBar: AppBar(title: const Text("路由demo"),),
       body: const Center(
         child: MyAppBarRouter(),
       ),
@@ -19,8 +19,13 @@ class MyAppBarRouter extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
       onPressed: () {
+        ///1.Navigator.of(context) 获得当前页面的导航器Navigator对象
+        ///2.MaterialPageRoute() 创建一个新路由,这里是Material风格的页面切换动画。
+        ///3.builder 参数是一个匿名方法,返回新的页面组件。这里返回一个SearchPage页面。
+        /// 4.push方法将新路由推入导航栈中,切换到SearchPage页面。
+        /// 5.const SearchPage() 构造并返回SearchPage组件,const表示这个组件是不变的。
         Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-          return SearchPage();
+          return const SearchPage(keyWord: "传递的参数",intVal: 1,);
         }));
       },
       icon: Icon(Icons.search),
--- lib/f3_router/T1_searchPage.dart
+++ lib/f3_router/T1_searchPage.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatefulWidget {
-  const SearchPage({Key? key}) : super(key: key);
+  final String keyWord;
+  final int intVal;
+  const SearchPage({Key? key,this.keyWord="",required this.intVal}) : super(key: key);
 
   @override
   _SearchPageState createState() => _SearchPageState();
@@ -10,10 +11,31 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('上个页面传递过来的参数:${widget.intVal},${widget.keyWord}');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        //返回上一页
+        ///具体Navigator.pop()的用法:
+        /// 简单返回上一页
+        /// Navigator.pop(context);
+        /// 返回上一页时传递数据
+        /// Navigator.pop(context, data);
+        /// 返回指定页面,弹出中间页面
+        /// Navigator.popUntil(context, ModalRoute.withName('/HomePage'));
+        Navigator.pop(context);
+      },child: Icon(Icons.back_hand),),
       appBar: AppBar(title: Text("搜索页"),),
-      body: Center(child: Text("我是搜索页"),),
+      body: Center(child: ListView(children: [
+        ListTile(title: Text("测试内容"),),
+        ListTile(title: Text(widget.keyWord),)
+      ],),),
     );
   }
 }
